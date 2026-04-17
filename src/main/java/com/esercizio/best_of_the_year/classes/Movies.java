a1e2ba2dbee1c80327edbd108d411ecfb37823e2
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-17 17:56:11 +0200

    resa classe movies Javabean

--- src/main/java/com/esercizio/best_of_the_year/classes/Movies.java
+++ src/main/java/com/esercizio/best_of_the_year/classes/Movies.java
@@ -1,12 +1,31 @@
 package com.esercizio.best_of_the_year.classes;
 
-public class Movies {
+import java.io.Serializable;
 
+public class Movies implements Serializable {
     private int id;
     private String title;
 
+    public Movies() {};
+
     public Movies(int id, String title) {
         this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setTitle(String title) {
         this.title = title;
     }
 }
