fecbd097f13c590424aa3f4d0b96ea705c3f5e2c
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-17 18:03:36 +0200

    navigando alla pagina /songs è ora possibile visualizzare anche la lista dei migliori film

--- src/main/java/com/esercizio/best_of_the_year/classes/Songs.java
+++ src/main/java/com/esercizio/best_of_the_year/classes/Songs.java
@@ -3,9 +3,27 @@ package com.esercizio.best_of_the_year.classes;
 public class Songs {
     private int id;
     private String title;
+    
+    public Songs() {};
 
     public Songs(int id, String title) {
         this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
     }
 }
